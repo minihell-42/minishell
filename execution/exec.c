6da1409cdcfb80659b497a35b5708ca66fee9251
samsnwn <samuel.c.glantz@gmail.com>
2025-03-01 20:32:17 +0100

    feat:init_exec

---
+++ execution/exec.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samcasti <samcasti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/01 20:03:02 by samcasti          #+#    #+#             */
+/*   Updated: 2025/03/01 20:30:14 by samcasti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	exec(t_node *node)
+{
+	if (node->type == "CMD")
+		exec_command(node);
+	else if (node->type == "PIPE")
+		exec_pipe(node);
+	else if (node->type == "SEQUENCE")
+		exec_sequence(node);
+}
