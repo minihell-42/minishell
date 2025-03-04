da06377ebea37fdab8b148c8f640a00baa416eff
samsnwn <samuel.c.glantz@gmail.com>
2025-03-04 18:55:40 +0100

    chore:norminette

--- srcs/execution/exec_command.c
+++ srcs/execution/exec_command.c
@@ -6,23 +6,23 @@
 /*   By: samcasti <samcasti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 20:11:08 by samcasti          #+#    #+#             */
-/*   Updated: 2025/03/03 18:17:53 by samcasti         ###   ########.fr       */
+/*   Updated: 2025/03/04 18:55:22 by samcasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/execution.h"
 
+// get token array from the tree and its length(argc)
+// populate argv with tokens and make the last element NULL
+// need to handle program path manually
+// pass this array to execve alongside envp
 int	exec_command(t_tree *tree, t_context *ctx, char **envp)
 {
 	int		pid;
-	char	*argv[2] = {"ls", 0};
+	char	*argv[2];
 	char	*program_path;
 
 	(void)tree;
-	// get token array from the tree and its length(argc)
-	// populate argv with tokens and make the last element NULL
-	// need to handle program path manually
-	// pass this array to execve alongside envp
 	pid = fork();
 	if (pid == FORKED_ERROR)
 		handle_fork_error();
