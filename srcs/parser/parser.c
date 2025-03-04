2d5cf1bd08741730045d9bb53de61d84e38a5aa5
Daniel Gomez Arango <dgomez-a@student.42berlin.de>
2025-03-04 13:05:23 +0100

    chore:Norminette

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,24 +6,24 @@
 /*   By: dgomez-a <dgomez-a@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 19:15:04 by dgomez-a          #+#    #+#             */
-/*   Updated: 2025/03/01 19:15:29 by dgomez-a         ###   ########.fr       */
+/*   Updated: 2025/03/04 13:03:46 by dgomez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-    Command to compile:
-    cc -I includes srcs/parser/parser.c -L/opt/homebrew/lib -lreadline -o minishell
+Command to compile:
+cc -I includes srcs/parser/parser.c -L/opt/homebrew/lib -lreadline -o minishell
 */
 #include "shell.h"
 
-int    main(void)
+int	main(void)
 {
-    char    *input;
+	char	*input;
 
-    input = readline("minishell$ ");
-    if (input == NULL)
-        exit(0);
-    lexer_tokenize(input);
-    free(input);
-    return (0);
+	input = readline("minishell$ ");
+	if (input == NULL)
+		exit(0);
+	lexer_tokenize(input);
+	free(input);
+	return (0);
 }
