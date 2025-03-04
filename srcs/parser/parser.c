e78c84203ead9630474fe9120a80f851770becee
Danielg1406 <danielgomeza1406@gmail.com>
2025-03-04 10:56:10 +0100

    feat:read input

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -9,3 +9,21 @@
 /*   Updated: 2025/03/01 19:15:29 by dgomez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+    Command to compile:
+    cc -I includes srcs/parser/parser.c -L/opt/homebrew/lib -lreadline -o minishell
+*/
+#include "shell.h"
+
+int    main(void)
+{
+    char    *input;
+
+    input = readline("minishell$ ");
+    if (input == NULL)
+        exit(0);
+    lexer_tokenize(input);
+    free(input);
+    return (0);
+}
