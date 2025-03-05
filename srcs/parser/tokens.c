0ea845dfff32e8c9c2c08e74aa6b7591e9902e78
Danielg1406 <danielgomeza1406@gmail.com>
2025-03-05 23:42:05 +0100

    feat:print tokens functino

--- srcs/parser/tokens.c
+++ srcs/parser/tokens.c
@@ -1,7 +1,19 @@
-#include "shell.h"
+#include "parser.h"
 
 t_token	*tokenize_redirections(char *input);
 
 t_token	*tokenize_pipes_and_separators(char *input);
 
-void	print_tokens(t_token *tokens);
+void	print_tokens(t_token *tokens)
+{
+  t_token	*current;
+
+  current = tokens;
+  while (current)
+  {
+    printf("start\n");
+    printf("Token: %s\n", current->value);
+    printf("Type: %d\n", current->type);
+    current = current->next;
+  }
+}
