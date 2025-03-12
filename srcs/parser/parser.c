07aed6b55e8e6309c8f63a32c35dde2d931440b0
Danielg1406 <danielgomeza1406@gmail.com>
2025-03-12 21:52:22 +0100

    feat:parse pipeline and redirections

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -16,8 +16,58 @@ cc -I includes srcs/parser/parser.c -L/opt/homebrew/lib -lreadline -o minishell
 */
 #include "parser.h"
 
-t_tree	*parse_tokens(t_token *tokens);
+/**
+ * Parses a sequence of tokens to build an abstract syntax tree (AST).
+ *
+ * @param tokens The input tokens to be parsed.
+ *
+ * @returns A pointer to the root of the AST representing the parsed tokens.
+ *          Returns NULL if the input tokens are empty or if syntax validation fails.
+ */
+t_tree	*parse_tokens(t_token *tokens)
+{
+	if (!tokens || tokens->type == TKN_END)
+		return (NULL);
+	if (!validate_syntax(tokens))
+		return (NULL);
+	return (parse_pipeline(&tokens));
+}
+
+/**
+ * Parses a pipeline of commands and creates an abstract syntax tree (AST) for the pipeline.
+ *
+ * @param tokens A pointer to the pointer to the tokens representing the pipeline.
+ *
+ * @returns A pointer to the root of the AST representing the pipeline.
+ */
+t_tree	*parse_pipeline(t_token **tokens)
+{
+	t_tree	*left_node;
+	t_tree	*pipe_node;
+
+	left_node = parse_command(tokens);
+	if (!left_node)
+		return (NULL);
+	if ((*tokens)->type == TKN_PIPE)
+	{
+		(*tokens) = (*tokens)->next;
+		pipe_node = create_ast_node(NODE_PIPE, NULL, 0, OTHER);
+		pipe_node->left = left_node;
+		pipe_node->right = parse_pipeline(tokens);
+		return (pipe_node);
+	}
+	return (left_node);
+}
+
+t_tree	*parse_redirection(t_token **tokens)
+{
+	t_tree				*redir_node;
+	t_redir_type	redir_type;
+	char					**argv;
+
+	if (!*tokens)
+		return (NULL);
+	
+}
+
 t_tree	*parse_command(t_token **tokens);
-t_tree	*parse_redirection(t_token **tokens);
-t_tree	*parse_pipeline(t_token **tokens);
-t_tree	*parse_sequence(t_token **tokens);
--- srcs/parser/parser_utils.c
+++ srcs/parser/parser_utils.c
@@ -12,8 +12,31 @@
 
 #include "parser.h"
 
-// TODO: Gives back the type of the command
-t_cmd_type	is_builtin(char *cmd);
+/**
+ * Determines if a given command is a built-in command.
+ *
+ * @param cmd The command to check.
+ *
+ * @returns The type of the built-in command.
+ */
+t_cmd_type	is_builtin(char *cmd)
+{
+	if (!ft_strcmp(cmd, "echo"))
+		return (ECHO);
+	if (!ft_strcmp(cmd, "cd"))
+		return (CD);
+	if (!ft_strcmp(cmd, "pwd"))
+		return (PWD);
+	if (!ft_strcmp(cmd, "export"))
+		return (EXPORT);
+	if (!ft_strcmp(cmd, "unset"))
+		return (UNSET);
+	if (!ft_strcmp(cmd, "env"))
+		return (ENV);
+	if (!ft_strcmp(cmd, "exit"))
+		return (EXIT);
+	return (OTHER);
+}
 
-// TODO: Checks for syntax errors before parsing (e.x | |, > <, etc)
-int			validate_syntax(t_token *tokens);
+// TODO: Checks for syntax errors before parsing (e.g., | |, > <, | >, etc.)
+int	validate_syntax(t_token *tokens);
