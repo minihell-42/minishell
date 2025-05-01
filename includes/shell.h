560b3058aff1914c647fcb5eb20253d812c29987
Danielg1406 <danielgomeza1406@gmail.com>
2025-05-01 16:35:33 +0200

    fix:Makefile to work in mac

--- includes/shell.h
+++ includes/shell.h
@@ -16,21 +16,21 @@
 # include "../libft/include/libft.h"
 # include "execution.h"
 # include "parser.h"
-# include <linux/limits.h>
+// # include <linux/limits.h>
 # include <errno.h>
 # include <fcntl.h>
 # include <limits.h>
-# include <readline/history.h>
-# include <readline/readline.h>
+# include <stdio.h>
 # include <signal.h>
 # include <stdbool.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 # include <sys/stat.h>
 # include <sys/types.h>
 # include <sys/wait.h>
 # include <unistd.h>
+# include <readline/history.h>
+# include <readline/readline.h>
 
 // SIGNALS
 void	setup_signals(void);
