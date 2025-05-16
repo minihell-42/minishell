276683ad26a054c81d7a1c8b9dd1b14497985c78
Daniel Gomez Arango <dgomez-a@student.42berlin.de>
2025-05-16 15:30:20 +0200

    feat:echo with multiple n flags

--- srcs/execution/built-ins/echo.c
+++ srcs/execution/built-ins/echo.c
@@ -16,20 +16,25 @@ int	builtin_echo(int argc, char **argv)
 {
 	int	i;
 	int	n_flag;
+	int	j;
 
 	i = 1;
 	n_flag = 0;
-	if (argc > 1 && !ft_strncmp(argv[1], "-n", 3))
+	while (i < argc && argv[i][0] == '-' && argv[i][1] == 'n')
 	{
+		j = 2;
+		while (argv[i][j] == 'n')
+			j++;
+		if (argv[i][j] != '\0')
+			break ;
 		n_flag = 1;
-		i = 2;
+		i++;
 	}
 	while (i < argc)
 	{
 		ft_putstr_fd(argv[i], STDOUT_FILENO);
-		if (i < argc - 1)
+		if (++i < argc)
 			ft_putchar_fd(' ', STDOUT_FILENO);
-		i++;
 	}
 	if (!n_flag)
 		ft_putchar_fd('\n', STDOUT_FILENO);
